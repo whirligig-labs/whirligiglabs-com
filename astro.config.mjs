a60e3b0367c32994200bdb79cc82917291a15b72
Chris Griffing <cmgriffing@gmail.com>
2022-08-26 14:27:23 -0700

    feat: add default social image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://whirligiglabs.netlify.app",
+  site: "https://whirligiglabs.com",
   integrations: [mdx(), solid(), tailwind()],
   markdown: {
     shikiConfig: {
